170fa84a1dd80e5581d38208368b0a192e8961ae
Dylan Khuu <khuudylan@gmail.com>
2026-02-26 11:19:15 -0700

    created tabs folder

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,57 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+	return (
+		<Tabs
+			screenOptions={{
+				headerShown: false,
+				tabBarStyle: { height: 60 },
+				tabBarActiveTintColor: "blue",
+				tabBarInactiveTintColor: "gray",
+			}}
+		>
+			<Tabs.Screen
+				name="home"
+				options={{
+					title: "Home",
+					tabBarIcon: ({ color, size, focused }) => (
+						<Ionicons
+							name={focused ? "home" : "home-outline"}
+							size={size}
+							color={color}
+						/>
+					),
+				}}
+			/>
+
+			<Tabs.Screen
+				name="search"
+				options={{
+					title: "Search",
+					tabBarIcon: ({ color, size, focused }) => (
+						<Ionicons
+							name={focused ? "search" : "search-outline"}
+							size={size}
+							color={color}
+						/>
+					),
+				}}
+			/>
+
+			<Tabs.Screen
+				name="notifications"
+				options={{
+					title: "Notifications",
+					tabBarIcon: ({ color, size, focused }) => (
+						<Ionicons
+							name={focused ? "notifications" : "notifications-outline"}
+							size={size}
+							color={color}
+						/>
+					),
+				}}
+			/>
+		</Tabs>
+	);
+}
