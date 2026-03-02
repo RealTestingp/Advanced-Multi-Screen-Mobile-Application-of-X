ebaa58580b089abd346faf8c7378a967e3d3f92d
Dylan Khuu <khuudylan@gmail.com>
2026-03-01 17:24:34 -0700

    added tabUnderline to notification tab

--- app/(tabs)/notifications/index.tsx
+++ app/(tabs)/notifications/index.tsx
@@ -63,6 +63,7 @@ export default function Notifications() {
 							>
 								{tab}
 							</Text>
+							{activeTab === tab && <View style={styles.tabUnderline} />}
 						</TouchableOpacity>
 					))}
 				</View>
